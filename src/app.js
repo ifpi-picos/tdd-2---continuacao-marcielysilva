f87a4092029c66bf02ed4ec32cf608a645d110d9
Marciely Ferreira <89710284+marcielysilva@users.noreply.github.com>
2023-06-19 21:36:27 -0300

    Api

--- src/app.js
+++ src/app.js
@@ -1,24 +1,34 @@
 const express = require('express');
+
 const app = express();
 app.use(express.json());
 
 const Tarefas = require('./tarefas');
 const tarefas = new Tarefas();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.post('/tarefas', (req, res) => {
-
-});
-
-app.delete('/tarefas/:indice', (req, res) => {
-
+  const { descricao } = req.body;
+  tarefas.adicionar(descricao);
+  res.status(201).send('Tarefa adicionada com sucesso!');
 });
 
 app.get('/tarefas', (req, res) => {
-
+  res.status(200).json(tarefas.lista);
 });
 
 app.patch('/tarefas/:indice', (req, res) => {
+  const { indice } = req.params;
+  tarefas.marcarConcluida(indice);
+  res.status(200).send('Tarefa marcada como concluída!');
+});
 
+app.delete('/tarefas/:indice', (req, res) => {
+  const { indice } = req.params;
+  tarefas.remover(indice);
+  res.status(200).send('Tarefa removida com sucesso!');
 });
 
 module.exports = app;
--- src/tarefas.js
+++ src/tarefas.js
@@ -1,24 +1,24 @@
 class Tarefas {
-    constructor() {
-      this.lista = [];
-    }
-  
-    adicionar(descricao) {
-      
-    }
-  
-    remover(indice) {
-      
-    }
-  
-    todas() {
-      
-    }
-  
-    marcarConcluida(indice) {
-     
-    }
+  constructor() {
+    this.lista = [];
   }
-  
+
+  adicionar(descricao) {
+    this.lista.push({descricao, concluida: false});
+  }
+
+  remover(indice) {
+    this.lista.splice(indice, 1);
+  }
+
+  todas() {
+    return this.lista;
+  }
+
+  marcarConcluida(indice) {
+   this.lista[indice].concluida = true;
+  }
+}
+
 module.exports = Tarefas;
   
